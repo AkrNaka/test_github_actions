58b8cd90a31273eab1019e66f4d404c51f6ccfbb
AkrNaka <naka@methodplus.co.jp>
2024-12-25 14:40:03 +0900

    testしてみたよ

---
+++ .github/scripts/test01.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "testしてみたよ"
+exit 0
---
+++ product.php
@@ -0,0 +1,54 @@
+<?php
+
+class Product
+{
+    private $name;
+    private $price;
+    public function __construct(NameInterface $name, PriceInterface $price)
+    {
+        $this->name = $name->getName();
+        $this->price = $price->getPrice();
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
+
+interface NameInterface
+{
+    public function getName(): string;
+}
+
+interface PriceInterface
+{
+    public function getPrice(): float;
+}
+
+class Name implements NameInterface
+{
+    public function getName(): string
+    {
+        return 'test';
+    }
+}
+class Price implements PriceInterface
+{
+    public function getPrice(): float
+    {
+        return 1234.5;
+    }
+}
+
+$name = new Name();
+$price = new Price();
+
+$product = new Product($name, $price);
+
+echo "インスタンス化の結果" . PHP_EOL;
+echo $product->getName() . PHP_EOL;
+echo $product->getPrice() . PHP_EOL;
